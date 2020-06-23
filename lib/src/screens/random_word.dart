0870db95190c5ae05766d8d41e4d29ea54090b9e
Md. Yousuf Ali <yousuf.csebd@gmail.com>
2020-06-23 18:59:54 +0600

    Handled save and remove

--- lib/src/screens/random_word.dart
+++ lib/src/screens/random_word.dart
@@ -7,6 +7,7 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _saved = <WordPair>[];
   final _suggestions = <WordPair>[];
   final _biggerFont = TextStyle(fontSize: 18.0);
 
@@ -28,11 +29,26 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+
+      onTap: () {
+        setState(() {
+          if(alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
